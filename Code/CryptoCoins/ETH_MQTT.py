91ef23911bf8046aa94e9944ff90b3f1ec484eea
TumiSavage03 <128997013+TumiSavage03@users.noreply.github.com>
2024-04-03 00:54:27 +0100

    Update ETH_MQTT.py
    Tumelo's part of the code

--- Code/CryptoCoins/ETH_MQTT.py
+++ Code/CryptoCoins/ETH_MQTT.py
@@ -23,3 +23,27 @@ def get_eth_price(label):
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
+        
+       # Extract price
+        eth_price = float(data['price'])
+        eth_price = "{:.2f}".format(eth_price)
+
+        # Check if this is not the first update (i.e., there's a previous value)
+        if 'prev_value' in get_eth_price.__dict__:
+            # Compare with the previous value
+            prev_value = get_eth_price.prev_value
+            if eth_price > prev_value: # New Price is Greater, Green
+                label.config(fg='green')
+            elif eth_price < prev_value:   # New Price is Less than, Red
+                label.config(fg='red')
+        
+        # Update the label text with the price
+        label.config(text=f'ETHUSDT Price: ${eth_price}')
+
+        # Save the current value for the next comparison
+        get_eth_price.prev_value = eth_price
+
+        return eth_price
+
+    else:
+        label.config(text=f'Error: {response.status_code} - {response.text}')
